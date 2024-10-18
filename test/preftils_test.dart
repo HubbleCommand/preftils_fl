dcb7d10330e02a53612b2d622aa6564b66953495
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-10-18 21:57:06 +0200

    fix unit test

--- test/preftils_test.dart
+++ test/preftils_test.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   test('validate int', () async {
     SharedPreferences.setMockInitialValues({});
-    Preference<int> intpref = Preference("integer");
+    Preference<int> intpref = Preference("integer", 3);
 
     expect(await intpref.get(), 3);
 
